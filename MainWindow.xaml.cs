77b668898dc1aedab4f7933114a11e5f75e49c50
Maxim <mkolodyazhny@gmail.com>
2021-11-05 13:12:49 +0300

    calculates while going

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,13 +11,13 @@ namespace Swing
     public partial class Clock : Window
     {
         private double Mass = 1;
-        private double NominalLength = 1;
+        private double NominalLength = 40;
         private double Length = 1;
         private double DampCoef = 0.5;
-        private double Angle = 45, AngVel = 0, AngAcc = 0;
+        private double Angle = 45, AngVel = 0;
         private double step = 0.1;
         private double HourAngle = 0, MinuteAngle = 0, SecondAngle = 0;
-        private  double dH = GetRad(360 / Math.Pow(60, 3)), dM = GetRad(360 / Math.Pow(60, 2)), dS = GetRad(360 / 60);
+        private double dH = GetRad(360 / Math.Pow(60, 3)), dM = GetRad(360 / Math.Pow(60, 2)), dS = GetRad(360 / 60);
         private Queue<double> track = new Queue<double>();
         Polyline pl = new Polyline();
         const double g = 9.81;
@@ -27,24 +27,28 @@ namespace Swing
         }
         private void btnStart_Click(object sender, RoutedEventArgs e)
         {
+            track.Clear();
             HourAngle = 0; MinuteAngle = 0; SecondAngle = 0;
-            Mass = Double.Parse(tbMass.Text);
-            Length = Double.Parse(tbLength.Text);
-            NominalLength = 40;
-            DampCoef = Double.Parse(tbDamping.Text);
-            Angle = GetRad(Double.Parse(tbAngle.Text));
-            AngVel = 0; AngAcc = 0;
+            //impossible to enter mass/length/dampcoef < 0
+            Mass = Math.Abs(Double.Parse(tbMass.Text));
+            tbMass.Text = Mass.ToString();
+            Length = Math.Abs(Double.Parse(tbLength.Text));
+            tbLength.Text = Length.ToString();
+            DampCoef = Math.Abs(Double.Parse(tbDamping.Text));
+            tbDamping.Text = DampCoef.ToString();
+            Angle = GetRad(Double.Parse(tbAngle.Text)); AngVel = 0;
             tbDisplay.Text = "Moving...";
-            GetTrack();
+            //GetTrack();
             pl = new Polyline();
             pl.Stroke = Brushes.Red;
+            CompositionTarget.Rendering -= StartAnimation;
             CompositionTarget.Rendering += StartAnimation;
         }
 
         private void StartAnimation(object sender, EventArgs e)
         {
-            //get trajectory
-            if(track.Count > 0) Angle = track.Dequeue();
+            //get trajectory's new angle
+            GetTrack();
             // Display moving pendulum on screen:
             Point pt = new Point(
             PendulumLine.X1 + NominalLength * Math.Sin(Angle),
@@ -57,7 +61,7 @@ namespace Swing
             HourHand.X2 = 140 + 20 * Math.Sin(HourAngle); HourHand.Y2 = 42 - 20 * Math.Cos(HourAngle);
             MinuteHand.X2 = 140 + 20 * Math.Sin(MinuteAngle); MinuteHand.Y2 = 42 - 20 * Math.Cos(MinuteAngle);
             SecondHand.X2 = 140 + 20 * Math.Sin(SecondAngle); SecondHand.Y2 = 42 - 20 * Math.Cos(SecondAngle);
-            if (track.Count == 0)
+            if (Math.Abs(Angle) <= 0.01 && Math.Abs(AngVel) <= 0.01)
             {
                 tbDisplay.Text = "Stopped";
                 CompositionTarget.Rendering -= StartAnimation;
@@ -65,12 +69,12 @@ namespace Swing
         }
         private void btnReset_Click(object sender, RoutedEventArgs e)
         {
-            PendulumInitialize();
+            ClockInitialize();
             tbDisplay.Text = "Stopped";
             CompositionTarget.Rendering -= StartAnimation;
         }
 
-        private void PendulumInitialize()
+        private void ClockInitialize()
         {
             track.Clear();
             tbMass.Text = "1";
@@ -93,21 +97,19 @@ namespace Swing
             CompositionTarget.Rendering -= StartAnimation;
             track.Clear();
         }
-        private double sec_der (double phi, double v) { return -(DampCoef*v/(Mass*Length) + g*Math.Sin(phi)/Length); }
+        private double sec_der (double phi, double v) { 
+            return -(DampCoef*v/(Mass*Length) + g*Math.Sin(phi)/Length);
+        }
         static double GetRad(double phi) { return phi * Math.PI / 180; }
-        private void GetTrack()
+        private void GetTrack() //using Runge-Kutt for second grade dif eq
         {
             double K_1a, K_2a, K_3a, K_4a, K_1z, K_2z, K_3z, K_4z;
-            for(double t = 0; Math.Abs(Angle) >= 0.01 || Math.Abs(AngVel) >= 0.01; t += step)
-            {
-                track.Enqueue(Angle);
-                K_1a = AngVel; K_1z = sec_der(Angle, AngVel);
-                K_2a = AngVel + step * K_1z / 2; K_2z = sec_der(Angle + step * K_1a / 2, AngVel + step * K_1z / 2);
-                K_3a = AngVel + step * K_2z / 2; K_3z = sec_der(Angle + step * K_2a / 2, AngVel + step * K_2z / 2);
-                K_4a = AngVel + step * K_3z; K_4z = sec_der(Angle + step * K_3a, AngVel + step * K_3z);
-                Angle += step * (K_1a + 2 * K_2a + 2 * K_3a + K_4a) / 6;
-                AngVel += step * (K_1z + 2 * K_2z + 2 * K_3z + K_4z) / 6;
-            }
+            K_1a = AngVel; K_1z = sec_der(Angle, AngVel);
+            K_2a = AngVel + step * K_1z / 2; K_2z = sec_der(Angle + step * K_1a / 2, AngVel + step * K_1z / 2);
+            K_3a = AngVel + step * K_2z / 2; K_3z = sec_der(Angle + step * K_2a / 2, AngVel + step * K_2z / 2);
+            K_4a = AngVel + step * K_3z; K_4z = sec_der(Angle + step * K_3a, AngVel + step * K_3z);
+            Angle += step * (K_1a + 2 * K_2a + 2 * K_3a + K_4a) / 6;
+            AngVel += step * (K_1z + 2 * K_2z + 2 * K_3z + K_4z) / 6;
         }
     }
 }
